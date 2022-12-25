423db2ffdc42fce26360e5c85228671f8fb07cc8
Yehuda Lehrfield <yll2113@columbia.edu>
2022-12-25 12:28:10 -0500

    fix: unit test, use puzzle strings from controller folder

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,35 +2,29 @@ const chai = require("chai");
 const assert = chai.assert;
 
 const Solver = require("../controllers/sudoku-solver.js");
+const {
+  validPuzzlesAndSolutions,
+  testingPuzzleStringsAndSolution,
+} = require("../controllers/puzzle-strings");
 let solver = new Solver();
 
 suite("Unit Tests", () => {
-  // valid, invalid chars, invalid length
-  let puzzleStrings = [
-    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
-    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9...a..1945....4.37.4.3..6..",
-    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9.....1945....4.37.4.3..6..",
-    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9.....1945....4.37.4.3..6....",
-  ];
-  let puzzleSolution =
-    "769235418769235418769235418769235418769235418769235418769235418769235418769235418";
-
   suite("Validation", () => {
     test("Valid Puzzle String", (done) => {
       assert.isTrue(
-        solver.validate(puzzleStrings[0]),
+        solver.validate(testingPuzzleStringsAndSolution[0]),
         "Valid puzzle string should be true"
       );
       done();
     });
     test("Invalid Characters", (done) => {
       assert.property(
-        solver.validate(puzzleStrings[1]),
+        solver.validate(testingPuzzleStringsAndSolution[1]),
         "error",
         "Should have an error"
       );
       assert.equal(
-        solver.validate(puzzleStrings[1]).error,
+        solver.validate(testingPuzzleStringsAndSolution[1]).error,
         "Invalid characters in puzzle",
         "Should invalidate for invalid characters"
       );
@@ -38,12 +32,12 @@ suite("Unit Tests", () => {
     });
     test("Less than 81 characters", (done) => {
       assert.property(
-        solver.validate(puzzleStrings[2]),
+        solver.validate(testingPuzzleStringsAndSolution[2]),
         "error",
         "Should have an error"
       );
       assert.equal(
-        solver.validate(puzzleStrings[2]).error,
+        solver.validate(testingPuzzleStringsAndSolution[2]).error,
         "Expected puzzle to be 81 characters long",
         "Should invalidate for string less than 81 characters"
       );
@@ -51,12 +45,12 @@ suite("Unit Tests", () => {
     });
     test("More than 81 characters", (done) => {
       assert.property(
-        solver.validate(puzzleStrings[3]),
+        solver.validate(testingPuzzleStringsAndSolution[3]),
         "error",
         "Should have an error"
       );
       assert.equal(
-        solver.validate(puzzleStrings[3]).error,
+        solver.validate(testingPuzzleStringsAndSolution[3]).error,
         "Expected puzzle to be 81 characters long",
         "Should invalidate for string more than 81 characters"
       );
@@ -65,47 +59,72 @@ suite("Unit Tests", () => {
   });
   suite("Row Placement", () => {
     test("Valid Row Placement", (done) => {
-      assert.isTrue(solver.checkRowPlacement(puzzleStrings[0], 1, 1, 7));
+      assert.isTrue(
+        solver.checkRowPlacement(testingPuzzleStringsAndSolution[0], 1, 1, 7)
+      );
       done();
     });
     test("Invalid Row Placement", (done) => {
-      assert.isFalse(solver.checkRowPlacement(puzzleStrings[0], 8, 7, 5));
+      assert.isFalse(
+        solver.checkRowPlacement(testingPuzzleStringsAndSolution[0], 8, 7, 5)
+      );
       done();
     });
   });
   suite("Column Placement", () => {
     test("Valid Column Placement", (done) => {
-      assert.isTrue(solver.checkColPlacement(puzzleStrings[0], 2, 5, 9));
+      assert.isTrue(
+        solver.checkColPlacement(testingPuzzleStringsAndSolution[0], 2, 5, 9)
+      );
       done();
     });
     test("Invalid Column Placement", (done) => {
-      assert.isFalse(solver.checkColPlacement(puzzleStrings[0], 7, 3, 2));
+      assert.isFalse(
+        solver.checkColPlacement(testingPuzzleStringsAndSolution[0], 7, 3, 2)
+      );
       done();
     });
   });
   suite("Region Placement", () => {
     test("Valid Region Placement", (done) => {
-      assert.isTrue(solver.checkRegionPlacement(puzzleStrings[0], 2, 7, 3));
+      assert.isTrue(
+        solver.checkRegionPlacement(testingPuzzleStringsAndSolution[0], 2, 7, 3)
+      );
       done();
     });
     test("Valid Region Placement", (done) => {
-      assert.isFalse(solver.checkRegionPlacement(puzzleStrings[0], 6, 7, 3));
+      assert.isFalse(
+        solver.checkRegionPlacement(testingPuzzleStringsAndSolution[0], 6, 7, 3)
+      );
       done();
     });
   });
   suite("Puzzle Solution", () => {
     test("Valid Strings Pass Solver", (done) => {
-      assert.isArray(solver.solve(puzzleStrings[0]));
+      assert.property(
+        solver.solve(testingPuzzleStringsAndSolution[0]),
+        "solution"
+      );
       done();
     });
     test("Invalid Strings Do Not Pass Solver", (done) => {
-      assert.property(solver.solve(puzzleStrings[1]), "error");
+      assert.property(
+        solver.solve(testingPuzzleStringsAndSolution[1]),
+        "error"
+      );
       done();
     });
-    test("Valid Strings Pass Solver", (done) => {
+    test("Solver Returns expected solution", (done) => {
+      assert.property(
+        solver.solve(testingPuzzleStringsAndSolution[0]),
+        "solution"
+      );
       assert.equal(
-        solver.generateSolutionString(solver.solve(puzzleStrings[0])),
-        puzzleSolution
+        solver.solve(testingPuzzleStringsAndSolution[0]).solution,
+        testingPuzzleStringsAndSolution[5]
+      );
+      validPuzzlesAndSolutions.forEach((elem) =>
+        assert.equal(solver.solve(elem[0]).solution, elem[1])
       );
       done();
     });
